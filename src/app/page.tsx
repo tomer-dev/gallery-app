0abe10bbad1fc4e3459ace7184598185b08326c4
tomer-dev <tomercouch@gmail.com>
2025-01-06 00:52:10 +0100

    opting in to next cache control new model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { db } from "~/server/db";
 
-export const dynamic = "force-dynamic";
-
 export default async function HomePage() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
