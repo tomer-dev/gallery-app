c3dfdf0abbbe2e71ee39e686e5906ebad7289152
tomer-dev <tomer.sharoni@rookout.com>
2025-01-05 20:47:20 +0100

    scaffold ui

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## Todo
 
-- [ ] Make it deploy (vercel)
-- [ ] Scaffold basic ui with mock data
+- [x] Make it deploy (vercel)
+- [x] Scaffold basic ui with mock data
 - [ ] Actually set up a database (vercel postgres)
 - [ ] Attach database to UI
 - [ ] Add authentication (w/ clerk)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/O7TbaRB4D0JoULwyshhqD71nvkBSOKaZJq5pbt9gWNy8sdeY",
+  "https://utfs.io/f/O7TbaRB4D0JoXHhWQ9RJ26aR4jvI8NfyrQo0ACnxdMSbEU3B",
+  "https://utfs.io/f/O7TbaRB4D0Jo5w8oj107zu4TMhjpfr2XgmSRCNWV9ne0UkIy",
+  "https://utfs.io/f/O7TbaRB4D0Jo4eXqONry9oMn76mqFZvsQByPxieDAkYRzfX5",
+];
+
+const mockImages = [...mockUrls, ...mockUrls, ...mockUrls].map(
+  (url, index) => ({
+    id: index + 1,
+    url,
+  }),
+);
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} className="max-h-32 w-48 object-contain" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
