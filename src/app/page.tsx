6491f5c1e833142d6871a3aed280cdddb69b4007
tomer-dev <tomercouch@gmail.com>
2025-01-05 22:40:51 +0100

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/O7TbaRB4D0JoULwyshhqD71nvkBSOKaZJq5pbt9gWNy8sdeY",
@@ -14,10 +15,15 @@ const mockImages = [...mockUrls, ...mockUrls, ...mockUrls].map(
   }),
 );
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} className="max-h-32 w-48 object-contain" />
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,5 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon, NeonQueryFunction } from "@neondatabase/serverless";
 
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -9,10 +9,10 @@ import * as schema from "./schema";
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
+  conn: NeonQueryFunction<any, any> | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn = globalForDb.conn ?? neon(env.POSTGRES_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
