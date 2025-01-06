0b7fdc4b1724adff3164f4142db5930d1cae425b
tomer-dev <tomercouch@gmail.com>
2025-01-06 02:02:38 +0100

    Upload button wip

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
@@ -10,6 +16,10 @@ export default function TopNav() {
           <SignInButton>Sign In </SignInButton>
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => router.refresh()}
+          />
           <UserButton />
         </SignedIn>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
 
 import { type Metadata } from "next";
 
