a9d913b48a8780e32b869adde8c2d4661b0e66ff
tomer-dev <tomercouch@gmail.com>
2025-01-06 20:48:50 +0100

    Use Next Image

--- src/app/_components/CarnivalImages.tsx
+++ src/app/_components/CarnivalImages.tsx
@@ -1,13 +1,24 @@
+import Image from "next/image";
 import { getMyCarnivalImages } from "~/server/db/queries";
 
 export async function CarnivalImages() {
   const images = await getMyCarnivalImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} className="h-32 w-48" />
+        <div
+          key={image.id}
+          className="flex h-48 w-48 flex-col items-center justify-center"
+        >
+          <Image
+            src={image.url}
+            objectFit="fill"
+            alt={image.name}
+            width={192}
+            height={128}
+            style={{ objectFit: "fill" }}
+          />
           <div className="mx-auto">{image.name}</div>
         </div>
       ))}
