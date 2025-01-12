0847c59a1ec8bdd0a0ce5d4f266d264edcf1f59b
tomer-dev <tomercouch@gmail.com>
2025-01-12 16:49:27 +0100

    Message when no pictures are uploaded

--- src/app/_components/images.tsx
+++ src/app/_components/images.tsx
@@ -5,23 +5,27 @@ import { getMyImages } from "~/server/db/queries";
 export default async function Images() {
   const images = await getMyImages();
 
-  return images.map((image) => (
-    <Link
-      href={`/img/${image.id}`}
-      key={image.id}
-      draggable={false}
-      className="flex h-40 w-48 flex-col items-center justify-center"
-    >
-      <Image
-        src={image.url}
-        alt={image.name}
-        width={192}
-        height={120}
-        objectFit="cover"
-        style={{ height: "120px", maxHeight: "120px", objectFit: "cover" }}
-      />
+  return !images.length ? (
+    <span className="p-4 text-2xl">Please Upload pictures</span>
+  ) : (
+    images.map((image) => (
+      <Link
+        href={`/img/${image.id}`}
+        key={image.id}
+        draggable={false}
+        className="flex h-40 w-48 flex-col items-center justify-center"
+      >
+        <Image
+          src={image.url}
+          alt={image.name}
+          width={192}
+          height={120}
+          objectFit="cover"
+          style={{ height: "120px", maxHeight: "120px", objectFit: "cover" }}
+        />
 
-      <div className="mt-1 w-full truncate text-center">{image.name}</div>
-    </Link>
-  ));
+        <div className="mt-1 w-full truncate text-center">{image.name}</div>
+      </Link>
+    ))
+  );
 }
